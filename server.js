69aff1899d083b64a5cab2050d92278429b334ea
Allison Siegel <asiegel1@umd.edu>
2020-10-12 14:08:24 -0400

    brought in working labs

--- server.js
+++ server.js
@@ -22,11 +22,12 @@ app.use((req, res, next) => {
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
+    res.send(`Lab 5 for Allison Siegel`);
   })
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
+    res.send('hello world');
   });
 
 app.listen(port, () => {
